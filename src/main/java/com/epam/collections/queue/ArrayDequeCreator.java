e6bd03f81e12d623e7f418376f0be719bc7b77ef
Renata Yermak <renatkahturo@gmail.com>
2023-04-11 22:38:12 +0300

    .

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,20 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> result = new ArrayDeque<>();
+        result.add(firstQueue.remove());
+        result.add(firstQueue.remove());
+        result.add(secondQueue.remove());
+        result.add(secondQueue.remove());
+
+        while (!(firstQueue.isEmpty() && secondQueue.isEmpty())) {
+            firstQueue.add(result.removeLast());
+            result.add(firstQueue.remove());
+            result.add(firstQueue.remove());
+            secondQueue.add(result.removeLast());
+            result.add(secondQueue.remove());
+            result.add(secondQueue.remove());
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,30 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        Deque<Integer> dishQueue = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 1; i <= numberOfDishes; i++) {
+            dishQueue.add(i);
+        }
+
+        int i = 1;
+        while (!dishQueue.isEmpty()) {
+            if (i == everyDishNumberToEat) {
+                result.add(dishQueue.pollFirst());
+                i = 1;
+            } else {
+                Integer element = dishQueue.pollFirst();
+                dishQueue.addLast(element);
+                i++;
+            }
+        }
+        return result;
     }
 }
